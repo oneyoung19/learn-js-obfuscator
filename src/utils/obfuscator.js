ce2e7c4c02cf92dceda77beff39017ca612205ce
yangxiaoping <2878847069@qq.com>
2024-07-15 14:05:02 +0800

    chore: 更新依赖版本

--- src/utils/obfuscator.js
+++ src/utils/obfuscator.js
@@ -1,5 +1,10 @@
 /*
+[webpack-obfuscator](https://www.npmjs.com/package/webpack-obfuscator)
+
 [javascript-obfuscator](https://www.npmjs.com/package/javascript-obfuscator)
 
-[webpack-obfuscator](https://www.npmjs.com/package/webpack-obfuscator)
+webpack4:
+webpack-obfuscator@^2.6.0
+javascript-obfuscator@^2.6.0
+
 */
